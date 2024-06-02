4aa6ae020139754daa9bb96c9e8bbabde9193e86
Nitin pant <165370507+Nitinpant2004@users.noreply.github.com>
2024-06-02 19:49:40 +0530

    Add files via upload
    I am excited to share a project I recently completed: a fully functional calculator application developed using Python. This project allowed me to enhance my programming skills and apply various concepts in a practical setting.

---
+++ calculator.py
@@ -0,0 +1,14 @@
+num1=int(input("Enter a number: "))
+op = input("Enter an operator: ")
+num2=int(input("Enter another number: "))
+
+if  op=="+":
+  print(num1+num2)
+elif op=="-":
+  print(num1-num2)
+elif op=="*":
+  print(num1*num2)
+elif op=="/":
+  print(num1/num2)
+else:
+   print("something went wrong")
